938621e08f7acc1e14af6d97a6ba5c38e602ccef
voytoff <voytoff@gmail.com>
2026-05-13 14:56:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QApplication>
+#include <QDebug>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QStandardItemModel>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  app.setStyle("fusion");
+
+  MainWindow window;
+  window.show();
+  return app.exec();
+}
